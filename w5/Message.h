1c10c2df7c8ce7ff78a871032ff1b8dc020a1f9f
eiglikov <sneanone@gmail.com>
2015-10-20 22:30:37 -0400

    w5_gleb

---
+++ w5/Message.h
@@ -0,0 +1,19 @@
+//Message.h
+#pragma once
+#include <iostream>
+#include <fstream>
+
+namespace w5{
+class Message{
+    std::string user;
+    std::string reply;
+    std::string tweet;
+    
+    
+public:
+Message();
+Message(std::ifstream& in, char c);
+bool empty() const;
+void display(std::ostream& os) const;
+};
+}//w5
