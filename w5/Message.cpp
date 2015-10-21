d67f854c931d32ce3f471e4f3d1b07f680ae658a
eiglikov <sneanone@gmail.com>
2015-10-21 05:26:14 -0400

    Final version of w5

--- w5/Message.cpp
+++ w5/Message.cpp
@@ -3,57 +3,39 @@
 
 namespace w5 {
 
-    Message::Message() {
-    }
+    Message::Message() { } // returns true if the object is in a safe empty state
 
-    Message::Message(std::ifstream &in, char c) /*constructor retrieves a record from
-the in file object, parses the record (as described above) and stores 
-its components in the Message object. c is the character that delimits 
-each record*/
-    {
+    Message::Message(std::ifstream &in, char c) {
         if (!in.eof()) {
-            user = "Message\n User : ";
-            char x;
-            x = in.get();
-            while (x != ' ' && x != c && x != EOF) {
-                user += x;
-                x = in.get();
-            }
-            if (x == c)
-                reply = "";
-            else {
-                x = in.get();
-                if (x == '@') {
-                    reply += "Reply : ";
-                    while (x != ' ' && x != c && x != EOF) {
-                        x = in.get();
-                        reply += x;
-                    }
-                }
-                tweet += "Tweet : ";
-                while (x != c && x != EOF) {
-                    tweet += x;
-                    x = in.get();
-                }
+            getline(in, user, ' ');
 
+            if (in.peek() == '@') {
+                in.get();
+                getline(in, reply, ' ');
             }
+
+            getline(in, tweet, c);
         }
     }
 
-    bool Message::empty() const// returns true if the object is in a safe empty state
+    bool Message::empty() const // returns true if the object is in a safe empty state
     {
-        if (reply == "" && tweet == "") {
-            return true;
-        } else {
-            return false;
+        return reply == "" && tweet == "" ? true : false;
+    }
+
+    void Message::display(std::ostream &os) const // displays the Message objects within the container
+    {
+        if (!empty()) {
+            os << "Message\n User : ";
+            os << user << std::endl;
+
+            if (reply != "") {
+                os << "Reply : ";
+                os << reply << std::endl;
+            }
+            os << "Tweet : ";
+            os << tweet << std::endl;
         }
     }
 
-    void Message::display(std::ostream &os) const// displays the Message objects within the container
-    {
-        os << user << std::endl;
-        os << reply << std::endl;
-        os << tweet << std::endl;
-    }
-//w5
-}
+}//w5
--- w5/w5.cpp
+++ w5/w5.cpp
@@ -1,45 +1,45 @@
 // Workshop 5 - Containers
- // w5.cpp
+// w5.cpp
 
- #include <iostream>
- #include <fstream>
- #include "Message.h"
- #include "Notifications.h"
+#include <iostream>
+#include <fstream>
+#include "Message.h"
+#include "Notifications.h"
 
- const char REC_DELIMITER = '\n';
+const char REC_DELIMITER = '\n';
 
- w5::Notifications collect(std::ifstream& in, char recDelim) {
-     w5::Notifications notifications;
+w5::Notifications collect(std::ifstream& in, char recDelim) {
+    w5::Notifications notifications;
 
-     do {
-         w5::Message message(in, recDelim);
-         if (!message.empty())
-             notifications += std::move(message);
-     } while(in);
+    do {
+        w5::Message message(in, recDelim);
+        if (!message.empty())
+            notifications += std::move(message);
+    } while(in);
 
-     return notifications;
- }
+    return notifications;
+}
 
- int main(int argc, char* argv[]) {
-     if (argc == 1) {
-         std::cerr << argv[0] << ": missing file operand\n";
-         return 1;
-     }
-     else if (argc != 2) {
-         std::cerr << argv[0] << ": too many arguments\n";
-         return 2;
-     }
-     std::ifstream file(argv[1]);
-     if (!file) {
-         std::cerr << argv[0] << "\n: Failed to open " << argv[1] << "\n"; 
-         return 3;
-     }
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        std::cerr << argv[0] << ": missing file operand\n";
+        return 1;
+    }
+    else if (argc != 2) {
+        std::cerr << argv[0] << ": too many arguments\n";
+        return 2;
+    }
+    std::ifstream file(argv[1]);
+    if (!file) {
+        std::cerr << argv[0] << "\n: Failed to open " << argv[1] << "\n";
+        return 3;
+    }
 
-     std::cout << "\nNotifications\n=============\n\n";
-     w5::Notifications notifications = collect(file, REC_DELIMITER);
+    std::cout << "\nNotifications\n=============\n\n";
+    w5::Notifications notifications = collect(file, REC_DELIMITER);
 
-     notifications.display(std::cout);
+    notifications.display(std::cout);
 
-     std::cout << "Press any key to continue ... ";
-     std::cin.get();
- }
+    std::cout << "Press any key to continue ... ";
+    std::cin.get();
+}
